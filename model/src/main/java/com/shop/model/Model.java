cc8d0dabd670e52bc1da97f7a98a5dd11dfcedf2
afilkevich <afilkevich@ya.ru>
2017-04-21 15:54:16 +0300

    create Model.class in model

---
+++ model/src/main/java/com/shop/model/Model.java
@@ -0,0 +1,57 @@
+package com.shop.model;
+
+import java.util.Objects;
+
+/**
+ * Created by master on 21.4.17.
+ */
+public class Model {
+    private Integer id;
+    private String name;
+
+    public Model() {
+    }
+
+    public Model(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Model{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Model model = (Model) o;
+        return Objects.equals(id, model.id) &&
+                Objects.equals(name, model.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
