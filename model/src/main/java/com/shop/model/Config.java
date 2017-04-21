d8d6190f8f91048fbea777eab642eb83139e04af
afilkevich <afilkevich@ya.ru>
2017-04-21 15:58:28 +0300

    create Config.class in model

---
+++ model/src/main/java/com/shop/model/Config.java
@@ -0,0 +1,69 @@
+package com.shop.model;
+
+import java.util.Objects;
+
+/**
+ * Created by master on 21.4.17.
+ */
+public class Config {
+    private Integer id;
+    private String type;
+    private String descripction;
+
+    public Config(Integer id, String type, String descripction) {
+        this.id = id;
+        this.type = type;
+        this.descripction = descripction;
+    }
+
+    public Config() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescripction() {
+        return descripction;
+    }
+
+    public void setDescripction(String descripction) {
+        this.descripction = descripction;
+    }
+
+    @Override
+    public String toString() {
+        return "Config{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", descripction='" + descripction + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Config config = (Config) o;
+        return Objects.equals(id, config.id) &&
+                Objects.equals(type, config.type) &&
+                Objects.equals(descripction, config.descripction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, type, descripction);
+    }
+}
