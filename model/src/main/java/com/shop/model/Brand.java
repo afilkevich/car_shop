559ec691a1c084d0f03542da6ecf5ef57f2bcab4
afilkevich <afilkevich@ya.ru>
2017-04-21 15:50:30 +0300

    create Brand.class in model

---
+++ model/src/main/java/com/shop/model/Brand.java
@@ -0,0 +1,57 @@
+package com.shop.model;
+
+import java.util.Objects;
+
+/**
+ * Created by master on 21.4.17.
+ */
+public class Brand {
+    private Integer id;
+    private String name;
+
+    public Brand(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Brand() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Brand brand = (Brand) o;
+        return Objects.equals(id, brand.id) &&
+                Objects.equals(name, brand.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
