d3de8455a16e217816c9c564cd4fa62ae324be07
afilkevich <afilkevich@ya.ru>
2017-04-21 16:00:41 +0300

    create Discount.class in model

---
+++ model/src/main/java/com/shop/model/Discount.java
@@ -0,0 +1,57 @@
+package com.shop.model;
+
+import java.util.Objects;
+
+/**
+ * Created by master on 21.4.17.
+ */
+public class Discount {
+    private Integer id;
+    private Integer valueDiscount;
+
+    public Discount(Integer id, Integer valueDiscount) {
+        this.id = id;
+        this.valueDiscount = valueDiscount;
+    }
+
+    public Discount() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getValueDiscount() {
+        return valueDiscount;
+    }
+
+    public void setValueDiscount(Integer valueDiscount) {
+        this.valueDiscount = valueDiscount;
+    }
+
+    @Override
+    public String toString() {
+        return "Discount{" +
+                "id=" + id +
+                ", valueDiscount=" + valueDiscount +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Discount discount = (Discount) o;
+        return Objects.equals(id, discount.id) &&
+                Objects.equals(valueDiscount, discount.valueDiscount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, valueDiscount);
+    }
+}
