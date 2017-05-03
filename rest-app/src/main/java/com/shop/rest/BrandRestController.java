707ce9b32325cec13b459d6c36a6b8af1092e2df
afilkevich <afilkevich@ya.ru>
2017-05-03 16:17:21 +0300

    create some method in BrandRestController

--- rest-app/src/main/java/com/shop/rest/BrandRestController.java
+++ rest-app/src/main/java/com/shop/rest/BrandRestController.java
@@ -2,7 +2,10 @@ package com.shop.rest;
 
 import com.shop.model.Brand;
 import com.shop.service.BrandService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,17 +15,43 @@ import java.util.List;
  */
 @CrossOrigin
 @RestController
-@RequestMapping(value="/brand")
+@RequestMapping
 public class BrandRestController {
+
+    private static final Logger LOGGER= LogManager.getLogger();
+
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    @ExceptionHandler({IllegalArgumentException.class})
+    public String incorrestDataError() {
+        return "{  \"response\" : \"Incorrect Data Error\' }";
+    }
 
     @Autowired
     BrandService brandService;
 
-    // curl -v localhost:8088/brand
+    // curl -v localhost:8088/brands
     @ResponseBody
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Brand> getAllBrand(){
+    @RequestMapping(value="/brands",method = RequestMethod.GET)
+    public List<Brand> findAllBrand(){
+        LOGGER.debug("BrandRestController:findAllBrand");
         return brandService.findAll();
     }
+
+    // curl -v localhost:8088/brand/name/Toyota
+    @ResponseBody
+    @RequestMapping(value = "brand/name/{name}",method = RequestMethod.GET)
+    public Brand findBrandByName(@PathVariable(value = "name")String name){
+        LOGGER.debug("BrandRestController: findBrandByName");
+        return brandService.findByName(name);
+    }
+
+    // curl -v localhost:8088/brand/1
+    @ResponseBody
+    @RequestMapping(value = "brand/{id}",method = RequestMethod.GET)
+    public Brand findBrandById(@PathVariable(value = "id")Integer id){
+        LOGGER.debug("BrandRestController: findBrandById");
+        return brandService.findById(id);
+    }
+
 
 }
