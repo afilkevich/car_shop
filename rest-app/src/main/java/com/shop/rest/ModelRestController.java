6155757af191fca41ddefcea40150ae224fc66e5
afilkevich <afilkevich@ya.ru>
2017-05-04 10:50:38 +0300

    create ModelRestController and complete all  *Mocktest with EasyMock

--- rest-app/src/main/java/com/shop/rest/ModelRestController.java
+++ rest-app/src/main/java/com/shop/rest/ModelRestController.java
@@ -37,4 +37,37 @@ public class ModelRestController {
         LOGGER.debug("ModelRestController:findAllModel");
         return modelService.findAll();
     }
+    // curl -v localhost:8088/model/name/Sedan
+    @ResponseBody
+    @RequestMapping(value = "model/name/{name}",method = RequestMethod.GET)
+    public Model findModelByName(@PathVariable(value = "name")String name){
+        LOGGER.debug("ModelRestController: findModelByName");
+        return modelService.findByName(name);
+    }
+
+    // curl -v localhost:8088/model/1
+    @ResponseBody
+    @RequestMapping(value = "model/{id}",method = RequestMethod.GET)
+    public Model findModelById(@PathVariable(value = "id")Integer id){
+        LOGGER.debug("ModelRestController: findModelById");
+        return modelService.findById(id);
+    }
+
+    // curl -H "Content-Type: application/json" -X POST -d '{"name":"jeep"}' -v localhost:8088/model
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @RequestMapping(value="model",method = RequestMethod.POST)
+    public Integer addModel(@RequestBody Model model){
+        LOGGER.debug("ModelRestController:addModel",model);
+        return modelService.insert(model);
+    }
+
+    // curl -H "Content-Type: application/json" -X PUT -d '{"id":"2","name":"cross"}' -v localhost:8088/model
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    @RequestMapping(value = "model", method = RequestMethod.PUT)
+    public Integer updateModel(@RequestBody Model model){
+        LOGGER.debug("ModelRestController:updateModel",model);
+        return modelService.update(model);
+    }
 }
